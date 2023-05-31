e67367c6ba65a3a4050f1abfb895d8b2b5f64671
Kirill <fikfack806@mail.ru>
2023-05-31 15:29:02 +0400

    Задание на Эйлеров путь: сделал шаблон программы

---
+++ programs/Eller's path.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+}
