7eb7775244a7b905be852367e180f874e1698091
Kirill <fikfack806@mail.ru>
2023-05-31 15:47:04 +0400

    Задание на хэши: сделал шаблон программы

---
+++ projects/hashes/hashes.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "task = ";
+	int task; cin >> task;
+	switch (task)
+	{
+	/*case 1:
+		openhash();
+		break;
+	case 2:
+		closehash();
+		break;*/
+	default:
+		break;
+	}
+}
+
+
